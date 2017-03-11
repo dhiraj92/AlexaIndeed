d8960b864022175854d89efdbdc6c2ef415c18d8
Dhiraj Gurkhe <dhiraj92>
2017-03-11 15:28:45 -0700

    changes

--- indeedApi.py
+++ indeedApi.py
@@ -1,20 +1,62 @@
 from indeed import IndeedClient
+import pandas as pd
 
-client = IndeedClient(8836246992678581)
 
-
-def skill(l):
-    #print l
-    #print " AND ".join(l)
-    params = {
-        'q' : " AND ".join(l),
-        'l' : "phoenix",
-        'userip' : "1.2.3.4",
-        'useragent' : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)",
-        'limit' : "50"
-    }
+class indeed:   
     
-    search_response = client.search(**params)
-    return search_response['results']
+    #jobDataFrame 
     
-#print skill(["Python","Java"])
+    def __init__(self):
+        self.jobDataFrame= pd.DataFrame();
+        self.client = IndeedClient(8836246992678581);
+        
+    def skill(self,l):
+        #print l
+        #print " AND ".join(l)
+        params = {
+            'q' : " AND ".join(l),
+            'l' : "phoenix",
+            'userip' : "1.2.3.4",
+            'useragent' : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)",
+            'limit' : "25",
+            'start' : 0,
+            'highlight' : 1
+          
+        }
+        i = 25
+        search_response = self.client.search(**params)
+        results = []
+        while(i<100 and i<search_response['totalResults']):        
+            results += search_response['results']
+            params['start'] += 25
+            search_response = self.client.search(**params)
+            results += search_response['results']
+            i+=25
+            print (params['start'])
+        self.jobDataFrame = pd.DataFrame(results).drop_duplicates('jobkey')
+        self.jobDataFrame.to_csv("sample.csv",encoding='UTF-8')
+        return results
+    
+    
+    def similarJobs(self,job):
+        sampledf = pd.read_csv("sample.csv",encoding='UTF-8')        
+        jobNo = "e5c7b9cbf1a50268"
+        self.dataJob = sampledf.loc[sampledf['jobkey'] == jobNo]
+        df= sampledf[sampledf["jobkey"] != jobNo]
+        
+        df.ix[df.city == self.dataJob.city.iloc[0], ['city','country','formattedLocation','formattedLocationFull','state']] = 1
+        df.ix[df.city != 1, ['city','country','formattedLocation','formattedLocationFull','state']] = 0
+        df.ix[df.company == self.dataJob.city.iloc[0], ['company','country','formattedLocation','formattedLocationFull','state']] = 1
+        df.ix[df.city != 1, ['city','country','formattedLocation','formattedLocationFull','state']] = 0
+
+#        df['city'] = ['red' if x == 'Z' else 'green' for x in df['Set']]
+        del df['stations']
+        del df['Unnamed: 0']
+        import pdb; pdb.set_trace()
+        
+        return sampledf
+        
+indeed = indeed() 
+#res = indeed.skill(["Python"])
+
+df =  indeed.similarJobs("xyz")
